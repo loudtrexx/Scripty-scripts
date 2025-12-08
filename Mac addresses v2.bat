1f263c2fd6ab3c400f6e3b2ba9f4be21da00e361
loudtrexx <191014340+loudtrexx@users.noreply.github.com>
2025-12-08 19:18:00 +0200

    Change output file path and improve comments

--- Mac addresses v2.bat
+++ Mac addresses v2.bat
@@ -1,5 +1,6 @@
 @echo off
-set outputFile="\\10.6.128.12\Ohjelmat\Automaattiset mac osoitteet\output.txt"
+set outputFile="output.txt"
 wmic computersystem get name /format:list >> %outputFile%
 wmic nic where "NetConnectionID='Wi-Fi'" get MACAddress /format:list >> %outputFile%
-:: Formatointi on hieman huonosti tehty, muuten toimii
+
+:: Bad formatting for the output. Maybe fix later
