992895e8b090bf3d3df1d623cb14195140e6cacb
loudtrexx <191014340+loudtrexx@users.noreply.github.com>
2026-03-17 11:16:57 +0200

    Added partition table rebuilding
    With the retable parameter you can rebuild the partition table in case it seems somehow broken

--- format-usb.sh
+++ format-usb.sh
@@ -7,6 +7,8 @@
 devices=$(lsblk -dpno NAME,TRAN,RM | awk '$2=="usb" && $3=="1" {print $1}')
 FS=${1:-exfat}
 LBL=${2:-USB %02d} #%02d can be used to number the value (USB01, USB02...) vfat/fat32 doesn't allow longer names than 11 characters
+RETABLE=${3:-}
+
 case "$FS" in # You can select another type of fs in case exfat is not to your taste
     exfat)
         MKFS="mkfs.exfat" # Recommended
@@ -23,14 +25,15 @@ case "$FS" in # You can select another type of fs in case exfat is not to your t
     ntfs)
         MKFS="mkfs.ntfs"
         PARAM="-f -F -L"
-        ;;
+	;;
     help)
 	printf "Supported file systems: exfat (Default) | fat32 | ntfs | ext4\n
 	EVERYTHING ON ALL PLUGGED IN USB drives WILL BE ERASED!!! 
  Usage:\n
- ./format-usb.sh [FILE SYSTEM] [LABEL]\n
+ ./format-usb.sh [FILE SYSTEM] [LABEL] [RETABLE (Optional)]\n
  ./format-usb.sh fat32 \u2013 Format the USB drive as fat32\n
  ./format-usb.sh exfat \"Suzuka usb\" \u2013 Format the drive as exfat with the label \"Suzuka usb\"\n
+ ./format-usb.sh exfat \"\" retable \u2013 Format the exfat with the label \"USB01\" and rebuild the GPT partition table\n
 ./format-usb.sh \u2013 Format the drive as exfat with label USB01 (If multiple usbs the next one will be USB02)
 Encountered an error? Make sure you have ntfs-3g or exfatprogs installed\n"
 	exit 1
@@ -45,13 +48,13 @@ if type "$MKFS" >/dev/null 2>&1; then
 	:
 else
 	if [ "$MKFS" == "mkfs.exfat" ]; then
-		printf "exprogs is missing from your system! Please install it via your package manager!"
+		printf "exprogs is missing from your system! Please install it via your package manager!\n"
 		exit 1
 	elif [ "$MKFS" == "mkfs.ntfs" ]; then
-		printf "ntfs-3g is missing from your system! Please install it via your package manager!"
+		printf "ntfs-3g is missing from your system! Please install it via your package manager!\n"
 		exit 1
 	else
-		printf "Something went wrong trying to verify the existance of the provided file system utility for $FS."
+		printf "Something went wrong trying to verify the existance of the provided file system utility for $FS.\n"
 		exit 1
 	fi
 fi
@@ -63,6 +66,22 @@ fi
 
 i=1
 for dev in $devices; do
+    case "$RETABLE" in
+    	retable) # Has the user metioned that they want a retable? (Also stop crying about it we'll make a gpt table)
+    		sudo umount "$dev"
+		if type sgdisk >/dev/null 2>&1; then # If sgdisk is installed (Comes with gdisk)
+       		 sudo sgdisk --zap-all "$dev"
+        	 sudo sgdisk --clear "$dev" # If there are any errors consided dd /dev/zero $dev zero-disk parameter might apear in the future
+        	 sudo sgdisk -n 1:0:0 -t 1:8300 "$dev" # Make a single partition with the full capacity
+    		 sudo partprobe "$dev"
+		else
+        	 printf "Please install gdisk from your package manager\n"
+        	 exit 1
+		fi
+    		;;
+    	*)
+    	 :
+    esac
     label=$(printf "$LBL" "$i")
     echo "Formatting $dev as $label"
 
